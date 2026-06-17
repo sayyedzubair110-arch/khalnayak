44de9549f4ac060126697c3cd93209febe7addfc
sayyedzubair110-arch <sayyedzubair110@gmail.com>
2026-06-17 16:29:56 +0500

    Change event listener target from 'shopBtn' to 'btn'

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-document.getElementById("shopBtn").addEventListener("click", function () {
+document.getElementById("btn").addEventListener("click", () => {
     alert("Welcome to Rizwan Gaming Store!");
-});
-
-const buttons = document.querySelectorAll(".product-card button");
-
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        alert("Product added to cart!");
-    });
 });
